df8acf01439bca853ebfbf3dd1eea86ac64a3cd3
Raheemh224 <raheemh224@gmail.com>
2025-12-30 22:48:55 +0000

    update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"  
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
@@ -32,7 +47,7 @@ resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "10.0.0.0/0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
@@ -41,3 +56,9 @@ tags     = {
 }
   
 }
+
+resource "aws_route_table_association" "publicsubnet-route-association" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
