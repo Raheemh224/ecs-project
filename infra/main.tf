74cc6687b2fc49d843f934f0a8885d2efde588b9
Raheemh224 <raheemh224@gmail.com>
2025-12-30 21:56:52 +0000

    Update Terraform code and move .gitignore to root

--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,34 @@ resource "aws_subnet" "main" {
     Name = "PublicSubnet1"
   }
 }
+
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.5.0/24"
+
+  tags = {
+    Name = "PrivateSubnet1"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+tags     = {
+  Name   = "PublicRT"
+}
+  
+}
