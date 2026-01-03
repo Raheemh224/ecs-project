62dcd2616258c97351f395a8e8316ea8cecf14c3
Raheemh224 <raheemh224@gmail.com>
2026-01-03 20:34:00 +0000

    Added security group for ALB

--- infra/main.tf
+++ infra/main.tf
@@ -95,3 +95,33 @@ resource "aws_route_table_association" "private_route_association" {
   route_table_id = aws_route_table.Priv_RT.id
 
 }
+
+resource "aws_security_group" "ALB_SG" {
+  name        = "ALB_SG"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.CustomVPC.id
+
+  tags = {
+    Name = "ALB-SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.ALB_SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.ALB_SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
+
+
