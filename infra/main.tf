5af4f785b56364844b8269ae06b699692e036bac
Raheemh224 <raheemh224@gmail.com>
2026-01-06 21:48:30 +0000

    added route 53 record

--- infra/main.tf
+++ infra/main.tf
@@ -416,13 +416,22 @@ resource "aws_route53_zone" "route53_zone" {
 }
 
 resource "aws_route53_record" "route53_record" {
-  zone_id = data.aws_route53_zone.route53_zone.zone_id
-  name    = "tm.raheemscustomdomain"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.alb-app.dns_name
-    zone_id                = aws_lb.alb-app.zone_id
-    evaluate_target_health = true
+  for_each = {
+    for dvo in aws_acm_certificate.acm_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
   }
+  
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.route53_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.acm_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
 }
