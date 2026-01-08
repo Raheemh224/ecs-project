ff59dce6f389b6bde593e419e790f5e4714673e1
Raheemh224 <raheemh224@gmail.com>
2026-01-08 23:15:29 +0000

    Debugging target group

--- infra/main.tf
+++ infra/main.tf
@@ -89,14 +89,7 @@ resource "aws_vpc_security_group_ingress_rule" "https" {
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
-}
 
-resource "aws_vpc_security_group_ingress_rule" "custom" {
-  security_group_id = aws_security_group.ALB_SG.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 3000
-  ip_protocol       = "tcp"
-  to_port           = 3000
 }
 
 resource "aws_vpc_security_group_ingress_rule" "http" {
@@ -130,16 +123,7 @@ resource "aws_lb_target_group" "alb_tg" {
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.CustomVPC.id
-}
-resource "aws_lb_listener" "port80" {
-  load_balancer_arn = aws_lb.alb-app.arn
-  port              = "80"
-  protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ECSTG.arn
-  }
 }
 
 resource "aws_ecr_repository" "ecsapp" {
@@ -169,19 +153,7 @@ resource "aws_ecr_lifecycle_policy" "repo_policy" {
     }]
   })
 }
-resource "aws_lb_target_group" "ECS_TG"{
-  name        = "ECSTG"
-  port        = 3000
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_vpc.CustomVPC.id
-}
 
-resource "aws_lb_target_group_attachment" "ecs_tg_attachment" {
-  target_group_arn = aws_lb_target_group.ECS_TG.arn
-  target_id        = aws_lb.alb-app.arn
-  port             = 80
-}
 resource "aws_ecs_cluster" "ThreatComposer" {
   name = "Threat-Composer"
 
@@ -279,7 +251,7 @@ resource "aws_security_group" "ecstask_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "ecssg_ingress" {
   security_group_id = aws_security_group.ecstask_sg.id
-  referenced_security_group_id = aws_lb.alb-app.id
+  referenced_security_group_id = aws_security_group.ALB_SG.id
   from_port   = 3000
   ip_protocol = "tcp"
   to_port     = 3000
@@ -307,13 +279,13 @@ resource "aws_ecs_service" "ECS_Service" {
 
   network_configuration {
   subnets         = [aws_subnet.PublicSubnet1.id,aws_subnet.PublicSubnet2.id]
-  security_groups = [aws_security_group.ecs_task_sg.id]
+  security_groups = [aws_security_group.ecstask_sg.id]
   assign_public_ip = false
 }
 
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.ECS_TG.arn
+    target_group_arn = aws_lb_target_group.alb_tg.arn
     container_name   = "ecsapp"
     container_port   = 3000
   }
