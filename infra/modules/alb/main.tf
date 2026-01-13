051aaf680caf02270a7a326af3bcb43dc2b280c2
Raheemh224 <raheemh224@gmail.com>
2026-01-13 22:12:44 +0000

    added alb resource from root

---
+++ infra/modules/alb/main.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "ALB_SG" {
+  name        = "ALB_SG"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.CustomVPC.id
+
+  tags = {
+    Name = "ALB-SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.ALB_SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.ALB_SG.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_egress" {
+  security_group_id = aws_security_group.ALB_SG.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+resource "aws_lb" "alb-app" {
+  name               = "alb-app"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ALB_SG.id]
+  subnets = [ aws_subnet.PublicSubnet1.id,aws_subnet.PublicSubnet2.id]
+
+  enable_deletion_protection = true
+
+}
+
+
+resource "aws_lb_target_group" "alb_tg" {
+  name        = "alb-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.CustomVPC.id
+
+}
