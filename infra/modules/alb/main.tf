ddbca03b733da7513a37928f402aa1baf0262135
Raheemh224 <raheemh224@gmail.com>
2026-01-13 23:03:56 +0000

    Added listener

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -50,3 +50,19 @@ resource "aws_lb_target_group" "alb_tg" {
   vpc_id      = aws_vpc.CustomVPC.id
 
 }
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb-app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
